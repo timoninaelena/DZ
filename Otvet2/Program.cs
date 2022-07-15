fa1c1cddf6b2bcb385c07a0daf45abf7d149ce1a
Elena <elena.tim@bk.ru>
2022-07-15 10:32:00 +0300

    ответ2

---
+++ Otvet2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = Convert.ToInt32(Console.ReadLine()) ;
+int b =  Convert.ToInt32(Console.ReadLine()) ;
+
+int max = a ;
+int min = b ;
+if (a > max ) max = a ;
+if (b > max ) max = b ;
+if (a < min ) min = a ;
+if (b < min ) min = b ;
+
+Console.Write("max = ") ;
+Console.WriteLine( max ) ;
+
+Console.Write("min = ") ;
+Console.WriteLine( min ) ;
