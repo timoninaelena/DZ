9c7bcad226c0db85de771ad100e2307cd800c4cb
Elena <elena.tim@bk.ru>
2022-07-15 10:36:46 +0300

    ответ4

---
+++ Otvet4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Convert.ToInt32(Console.ReadLine()) ;
+int b =  Convert.ToInt32(Console.ReadLine())  ;
+int c =  Convert.ToInt32(Console.ReadLine())  ;
+
+int max = a ;
+
+if (a > max ) max = a ;
+if (b > max ) max = b ;
+if (c > max ) max = c ;
+
+Console.Write("max = ");
+Console.WriteLine( max ) ;
